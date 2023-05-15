7abcca57662dcc764094602b626500ca68a07244
thecarsoncloud <116397808+thecarsoncloud@users.noreply.github.com>
2023-05-15 15:50:05 -0500

    searchbar is overlayed with resium globe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './components/cesium/custom-cesium-styles.css';
 import { Viewer } from 'resium';
+import { Sun } from 'resium';
+import SearchBar from './components/SearchBar';
 
 function App() {
   const viewerOptions = {
@@ -16,11 +18,28 @@ function App() {
     selectionIndicator: false,
   };
 
+  const containerStyle = {
+    position: 'relative',
+    width: '100%',
+    height: '100vh',
+  };
+
+  const overlayStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: 1,
+  };
+
   return (
-    <div>
+    <div style={containerStyle}>
       <Viewer full options={viewerOptions}>
-        {/* Your other Resium components here */}
+        <Sun />
       </Viewer>
+      <div style={overlayStyle}>
+        <SearchBar />
+      </div>
     </div>
   );
 }
