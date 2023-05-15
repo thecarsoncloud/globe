5808bab9d1887f522873861f48dfbe76094957ad
thecarsoncloud <116397808+thecarsoncloud@users.noreply.github.com>
2023-05-15 14:05:52 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Viewer, Sun } from 'resium';
+
+function App() {
+  return (
+    <Viewer full>
+      <Sun />
+      
+    </Viewer>
+  );
+}
+
+export default App;
---
+++ src/components/Sun.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useCesium } from 'resium';
+import Cesium from 'cesium';
+
+function Sun() {
+  const { viewer } = useCesium();
+
+  useEffect(() => {
+    const sun = new Cesium.Sun();
+    sun.glowFactor = 1.0;
+    sun.show = true;
+    viewer.scene.sun = sun;
+
+    return () => {
+      // Clean up on unmount
+      viewer.scene.sun = undefined;
+    };
+  }, [viewer]);
+
+  return null; // The Sun component doesn't render any visible elements
+}
+
+export default Sun;
