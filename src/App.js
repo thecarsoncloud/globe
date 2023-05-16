4ac7bbeeb62fde34b29436ccf5325c9594045286
thecarsoncloud <carson@ciellie.com>
2023-05-16 15:09:37 -0500

    final_camera_position

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import { Viewer, Camera, RectangleGraphics, SkyBox, createCesiumComponent, Sun } from 'resium';
+import React, { useEffect, useRef } from 'react';
+import { Viewer, Camera, RectangleGraphics, createCesiumComponent, Sun } from 'resium';
 import { Cartesian3, Rectangle, SkyBox as CesiumSkyBox } from 'cesium';
 import './components/cesium/custom-cesium-styles.css';
 
 const ResiumSkyBox = createCesiumComponent(CesiumSkyBox);
 
 function App() {
+  const viewerRef = useRef(null);
+
+  useEffect(() => {
+    if (viewerRef.current) {
+      const viewer = viewerRef.current.cesiumElement;
+
+      // Define the camera angle
+      const cameraPosition = Cartesian3.fromDegrees(-80.031362, 25.083987, 1300000.0); // Florida coordinates
+      const cameraOrientation = {
+        pitch: -Math.PI / 6, // Adjust the pitch value to your desired angle
+        heading: -Math.PI / 5, // Adjust the heading value to your desired angle
+        roll: 0.4,
+      };
+
+      // Fly the camera to the desired angle
+      viewer.camera.flyTo({
+        destination: cameraPosition,
+        orientation: cameraOrientation,
+        duration: 3, // Animation duration in seconds
+      });
+    }
+  }, []);
+
   const viewerOptions = {
     homeButton: false,
     geocoder: false,
@@ -25,30 +48,14 @@ function App() {
     height: '100vh',
   };
 
-  const overlayStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    zIndex: 1,
-  };
-
-  // Define the camera angle
-  const cameraPosition = Cartesian3.fromDegrees(-98.5833333, 39.8333333, 5000000.0); // Adjust the zoom level by changing the last parameter
-  const cameraOrientation = {
-    pitch: -Math.PI / 4, // Adjust the pitch value to your desired angle
-    heading: 0, // Adjust the heading value to your desired angle
-    roll: 0,
-  };
+  // Set the position of the Sun in the top left
+  const sunPosition = Cartesian3.fromDegrees(-180, 90); // Adjust the coordinates as needed
 
   return (
     <div style={containerStyle}>
-      <Viewer full options={viewerOptions}>
-        <Sun />
-        <Camera
-          defaultDestination={cameraPosition}
-          defaultOrientation={cameraOrientation}
-        />
+      <Viewer full options={viewerOptions} ref={viewerRef}>
+        <Sun position={sunPosition} />
+        <Camera />
         <ResiumSkyBox show={true} sources={{ positiveX: 'path/to/posx.png', negativeX: 'path/to/negx.png', positiveY: 'path/to/posy.png', negativeY: 'path/to/negy.png', positiveZ: 'path/to/posz.png', negativeZ: 'path/to/negz.png' }} />
         <RectangleGraphics rectangle={Rectangle.fromDegrees(-180, -90, 180, 90)} fill={false} outline={false} />
         {/* Add other components as needed */}
